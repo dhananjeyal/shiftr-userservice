69c1044afde229d840537f3777bf797e3b5b924f
aravind <m.s.aravind@askpundit.com>
2020-04-13 14:41:55 +0530

    column rename fixes

--- src/components/driver/model/experience.model.js
+++ src/components/driver/model/experience.model.js
@@ -27,7 +27,7 @@ class ExperienceDetails extends BaseModel {
                 modelClass: Speciality,
                 join: {
                     from: 'SRU09_DRIVEREXP.SRU09_SPECIALITY_REFERENCE_N',
-                    to: 'sru12_driver_speciality.SRU09_DRIVEREXP_D'
+                    to: 'sru12_driver_speciality.SRU09_SPECIALITY_REFERENCE_N'
                 }
             }
         }
