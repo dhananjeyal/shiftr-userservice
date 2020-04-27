e3ea364a76cb9e447ca8692b20a2c7b81043f14d
prakash <v.prakash@batumbu.id>
2020-04-27 15:11:11 +0530

    column Removal

--- src/components/driver/model/driver.columns.js
+++ src/components/driver/model/driver.columns.js
@@ -29,8 +29,7 @@ export let driverFinancialColumns = [
 export let driverExperienceColumns = [   
     'SRU09_DRIVEREXP_D as driverExperienceId',
     'SRU09_DRIVEREXP.SRU09_SPECIALITY_REFERENCE_N as specialityReferenceNumber',
-    'SRU09_TYPE_N as experienceType',//Experience Type = CANADA | USA
-    'SRU09_LICENSE_TYPE_N as licenseType',//Experience Type = CANADA | USA
+    'SRU09_TYPE_N as experienceType',//Experience Type = CANADA | USA    
     'SRU09_TOTALEXP_N as totalExp',
     'SRU09_CURRENT_N as currentExp',
     "SRU09_DRIVEREXP.SRU03_USER_MASTER_D as driveruserId"
