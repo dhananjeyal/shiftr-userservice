f7c1f4c8cb7476861a73f4fe8043b545a2ce8897
sasikumar <sasikumar9566@gmail.com>
2020-08-29 03:14:59 +0530

    list api updates

--- src/components/user/user.controller.js
+++ src/components/user/user.controller.js
@@ -1413,7 +1413,7 @@ class UserController extends BaseController {
                         .orWhere("SRU03_FIRST_N", "LIKE", `%${search}%`)
                         .orWhere("SRU03_LAST_N", "LIKE", `%${search}%`)
                         .orWhereRaw(`CONCAT(SRU03_FIRST_N, ' ', SRU03_LAST_N) LIKE ?`, `%${search}%`)
-                        .where(builder2 => userType == UserRole.CUSTOMER_R && builder2.where(`SRU04_COMPANY_NAME_N`, "LIKE", `%${search}%`))
+                        .orWhere(builder2 => userType == UserRole.CUSTOMER_R && builder2.where(`SRU04_COMPANY_NAME_N`, "LIKE", `%${search}%`))
                 });
             }
 
